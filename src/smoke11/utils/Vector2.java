407fa04d0fb34696e38b85b3dda4fa86bb44361a
smoke11 <bierkionline@gmail.com>
2013-06-29 19:37:52 +0200

    Getters and setters for x and y

--- src/smoke11/utils/Vector2.java
+++ src/smoke11/utils/Vector2.java
@@ -1,6 +1,22 @@
 package smoke11.utils;
 
 public class Vector2 {
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
     private int x;
     private int y;
 
